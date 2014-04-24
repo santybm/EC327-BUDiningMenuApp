4a1a0ed6f2194274ec103d6bad8ab44cd051be1a
Santiago Beltran <sbeltran@bu.edu>
2014-04-24 13:29:10 -0400

    Initial Commit

---
+++ BUDiningMobileMenu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BUDiningMobileMenu
+//
+//  Created by Santiago Beltran on 4/24/14.
+//  Copyright (c) 2014 DevXApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BUDiningMobileMenu/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BUDiningMobileMenu
+//
+//  Created by Santiago Beltran on 4/24/14.
+//  Copyright (c) 2014 DevXApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
